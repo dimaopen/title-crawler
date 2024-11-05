4f88f147de51a008ab003322ea4fb674f4273e22
Dmitry Openkov <dimaopen@gmail.com>
2024-11-05 12:29:45 +0300

    Encode uri -> result as json objects

--- src/main/scala/webcrawler/Routes.scala
+++ src/main/scala/webcrawler/Routes.scala
@@ -3,10 +3,10 @@ package webcrawler
 import cats.effect.Concurrent
 import cats.syntax.all.*
 import io.circe.syntax.*
-import io.circe.{Decoder, Encoder}
+import io.circe.{Decoder, Encoder, Json}
 import org.http4s.circe.*
 import org.http4s.dsl.Http4sDsl
-import org.http4s.{EntityDecoder, EntityEncoder, HttpRoutes}
+import org.http4s.{EntityDecoder, EntityEncoder, HttpRoutes, Uri}
 import webcrawler.api.{CrawlerError, TitleRequest, TitleResponse}
 
 object Routes:
@@ -16,10 +16,14 @@ object Routes:
     (crawlerError.code, crawlerError.message)
   }
 
-  given Encoder[Either[CrawlerError, String]] = Encoder.instance {
-    case Right(title) => title.asJson
-    case Left(error: CrawlerError) => error.asJson
-  }
+  given Encoder[(Uri, Either[CrawlerError, String])] = (uriResult: (Uri, Either[CrawlerError, String])) =>
+    val (uri, result) = uriResult
+    val jsonResult = result match
+      case Left(error) => Json.obj("error" -> error.asJson)
+      case Right(title) => Json.obj("title" -> title.asJson)
+    Json.obj(
+      "uri" -> uri.asJson,
+    ).deepMerge(jsonResult)
 
   given [F[_] : Concurrent]: EntityEncoder[F, TitleResponse] = jsonEncoderOf[F, TitleResponse]
 
